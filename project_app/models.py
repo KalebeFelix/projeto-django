c75c45777009241c774ffc64ea7c703b845b5354
Kalebe Felix <kalebe2006cr@gmail.com>
2024-03-22 19:38:20 -0300

    feat: Start Project Django

---
+++ project_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+
+    # cria uma tabela no banco de dados
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # mostra o que aparece quando for printar
+        return self.text
+
