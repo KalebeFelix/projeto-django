cb1acecceb110bb4fff7bf4a131a1b44addd1b81
Kalebe Felix <kalebe2006cr@gmail.com>
2024-03-25 20:44:59 -0300

    feat: adding Signup page

--- users/views.py
+++ users/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm  
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save() # faz login e redireciona para a home
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('index'))
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
